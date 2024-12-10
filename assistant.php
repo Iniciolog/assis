f6ca6a6a71704d2a9defb9421e3fd4a21fb81de4
Viacheslav Lazarenko <162235586+Iniciolog@users.noreply.github.com>
2024-12-10 04:14:17 +0300

    Create assistant.php

---
+++ assistant.php
@@ -0,0 +1,4 @@
+curl "https://prod.monit0r.ru:8444/v1/assistants?order=desc&limit=20" \
+  -H "Content-Type: application/json" \
+  -H "Authorization: Bearer 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" \
+  -H "OpenAI-Beta: assistants=v2"
